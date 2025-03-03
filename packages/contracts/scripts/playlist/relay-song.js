fafd4e47b69ed5972b9c39f7cfb98a3fef2bb45c
turinglabs <sebastiano.cataudo@gmail.com>
2025-03-03 17:42:44 +0100

    fix link and relay signature

--- packages/contracts/scripts/playlist/relay-song.js
+++ packages/contracts/scripts/playlist/relay-song.js
@@ -10,22 +10,22 @@ async function main() {
     let wallet = new ethers.Wallet(configs.owner_key).connect(provider)
     const contract = new ethers.Contract(configs.contract_addresses.Playlist, ABI.abi, wallet)
     const { keys } = await derive(configs.owner_mnemonic, 2)
+    const songUri = "https://turinglabs.mypinata.cloud/ipfs/bafybeibiofinmptxj53lxjfv34pm5y7eky5p73te6lgzwtlawn3ekwx2pm"
     const userWallet = new ethers.Wallet(keys[1]).connect(provider)
     console.log("User wallet is: " + userWallet.address)
-    const signature = await userWallet.signMessage("Add song: SONG_URI")
+    const signature = await userWallet.signMessage("Add song: " + songUri)
     console.log("Signature is: " + signature)
     try {
-
         const result = await axios.post("https://hammerhead-app-34p34.ondigitalocean.app/relay", {
             functionName: "addSong",
             contract: 'playlist',
             address: userWallet.address,
             signature,
-            message: "Add song: SONG_URI",
+            message: "Add song: " + songUri,
             args: [
-                "SONG_URI",
-                "IMAGE_URI",
-                "TITLE",
+                songUri,
+                "https://turinglabs.mypinata.cloud/ipfs/bafkreiazcwdhzvfmeiafeaknvtngm37ihjwncet6vozxel7ulcxea5lmrq",
+                "GENTLESS 3 - ON BUSTING THE SOUND BARRIER",
                 ethers.utils.keccak256(ethers.utils.toUtf8Bytes("TAG")),
                 signature
             ]
--- packages/web3-audio-player/src/utils/Utils.ts
+++ packages/web3-audio-player/src/utils/Utils.ts
@@ -1,117 +1,123 @@
-
 import { LiveStreamPlatform } from "../interfaces/interface";
 
 // Helper to resolve IPFS URIs
 const resolveIpfsUri = (uri: string) => {
+  if (!uri) {
+    console.error("Invalid URI:", uri);
+    return undefined;
+  }
+  const resolvedUri = uri.startsWith("ipfs://")
+    ? `https://dweb.link/ipfs/${uri.slice(7)}`
+    : uri;
 
-    if (!uri) {
-        console.error("Invalid URI:", uri);
-        return undefined;
-    }
-    const resolvedUri = uri.startsWith("ipfs://")
-        ? `https://dweb.link/ipfs/${uri.slice(7)}`
-        : uri;
+  //Convert G Drive link for audio
+  if (resolvedUri.includes("drive.google.com")) {
+    //Convert to example: https://drive.usercontent.google.com/download?id=13NVZTAls2Cx-1snX8_lH8ZJJ4-MVmbNW&export=view&authuser=0
+    const fixUrl = resolvedUri.replace("&export=download", "");
+    console.log("fixUrl", fixUrl);
+    return fixUrl;
+  }
 
-    //Convert G Drive link for audio
-    if (resolvedUri.includes("drive.google.com")) {
-        //Convert to example: https://drive.usercontent.google.com/download?id=13NVZTAls2Cx-1snX8_lH8ZJJ4-MVmbNW&export=view&authuser=0
-        const fixUrl = resolvedUri.replace("&export=download", "");
-        console.log("fixUrl", fixUrl);
-        return fixUrl;
-    }
-
-    return resolvedUri;
+  return resolvedUri;
 };
 
-const resolveCloudLinkUrl = (uri: string, type: 'img' | 'audio') => {
-    try {
-        const baseUrl = process.env.REACT_APP_API_BASE_URL;
-        const getUrl = `${baseUrl}/resolve/cloude-storage-link?link=${uri}&type=${type}`;
-        return getUrl;
-    } catch (error) {
-        console.error("Error resolving cloud link URL:", error);
-        return type === 'img' ? "/img_placeholder.jpg" : "/mp3_placeholder.jpg";
-    }
-}
+const resolveCloudLinkUrl = (uri: string, type: "img" | "audio") => {
+  try {
+    const baseUrl = process.env.REACT_APP_API_BASE_URL;
+    const getUrl = `${baseUrl}/resolve/cloud-storage-link?link=${uri}&type=${type}`;
+    return getUrl;
+  } catch (error) {
+    console.error("Error resolving cloud link URL:", error);
+    return type === "img" ? "/img_placeholder.jpg" : "/mp3_placeholder.jpg";
+  }
+};
 
 const sanitizeUri = (uri: string) => {
-    if (!uri) {
-        console.error("Invalid URI:", uri);
-        return undefined;
-    }
+  if (!uri) {
+    console.error("Invalid URI:", uri);
+    return undefined;
+  }
 
-    if (uri.includes("dropbox.com")) {
-        return uri.replace("dropbox.com", "dl.dropboxusercontent.com");
-    }
-    return uri;
+  if (uri.includes("dropbox.com")) {
+    return uri.replace("dropbox.com", "dl.dropboxusercontent.com");
+  }
+  return uri;
 };
 
 const resolveStreamingLink = (uri: string) => {
-
-    try {
-
-        //YOUTUBE
-        if (uri.includes("youtube")) {
-            const url = new URL(uri);
-            const videoId = url.searchParams.get("v") || url.pathname.split('/').pop();
-            if (videoId) {
-                const embedUrl = `https://www.youtube.com/embed/${videoId}`;
-                console.log("embedUrl", embedUrl);
-                return embedUrl;
-            }
-        }
-
-        //TWITCH
-        if (uri.includes("twitch")) {
-            const url = new URL(uri);
-            const channel = url.searchParams.get("channel") || url.pathname.split('/')?.pop()?.replace("/", "");
-            if (channel) {
-                const embedUrl = `https://player.twitch.tv/?channel=${channel}&parent=${process.env.REACT_APP_BASE_URL}`;
-                console.log("embedUrl", embedUrl);
-                return embedUrl;
-            }
-        }
-
-        return uri;
-    } catch (error) {
-        console.error("Error parsing YouTube URL:", error);
-        return uri;
+  try {
+    //YOUTUBE
+    if (uri.includes("youtube")) {
+      const url = new URL(uri);
+      const videoId =
+        url.searchParams.get("v") || url.pathname.split("/").pop();
+      if (videoId) {
+        const embedUrl = `https://www.youtube.com/embed/${videoId}`;
+        console.log("embedUrl", embedUrl);
+        return embedUrl;
+      }
     }
 
-}
+    //TWITCH
+    if (uri.includes("twitch")) {
+      const url = new URL(uri);
+      const channel =
+        url.searchParams.get("channel") ||
+        url.pathname.split("/")?.pop()?.replace("/", "");
+      if (channel) {
+        const embedUrl = `https://player.twitch.tv/?channel=${channel}&parent=${process.env.REACT_APP_BASE_URL}`;
+        console.log("embedUrl", embedUrl);
+        return embedUrl;
+      }
+    }
+
+    return uri;
+  } catch (error) {
+    console.error("Error parsing YouTube URL:", error);
+    return uri;
+  }
+};
 
 const getLivePlatformFromUri = (uri: string) => {
-    if (uri.includes("youtube")) {
-        return LiveStreamPlatform.YOUTUBE;
-    }
-    if (uri.includes("twitch")) {
-        return LiveStreamPlatform.TWITCH;
-    }
-    //HSL
-    if (uri.includes(".m3u8") || uri.includes(".m3u")) {
-        return LiveStreamPlatform.HLS;
-    }
-    return LiveStreamPlatform.RADIO;
-}
+  if (uri.includes("youtube")) {
+    return LiveStreamPlatform.YOUTUBE;
+  }
+  if (uri.includes("twitch")) {
+    return LiveStreamPlatform.TWITCH;
+  }
+  //HSL
+  if (uri.includes(".m3u8") || uri.includes(".m3u")) {
+    return LiveStreamPlatform.HLS;
+  }
+  return LiveStreamPlatform.RADIO;
+};
 
-const allowedUri = [ "drive.google", "dropbox", "dl.dropboxusercontent", "ipfs" ]
+const allowedUri = ["drive.google", "dropbox", "dl.dropboxusercontent", "ipfs"];
 const isSubmitUriFromAllowedPlatforms = (uri: string) => {
-    for(const uriPlatform of allowedUri){
-        if(uri.includes(uriPlatform)){
-            return true;
-        }
+  for (const uriPlatform of allowedUri) {
+    if (uri.includes(uriPlatform)) {
+      return true;
     }
-    return false;
-}
+  }
+  return false;
+};
 
-const allowedPlatforms = [ "youtube", "twitch" ]
+const allowedPlatforms = ["youtube", "twitch"];
 const isLiveUriFromAllowedPlatforms = (uri: string) => {
-    for(const platform of allowedPlatforms){
-        if(uri.includes(platform)){
-            return true;
-        }
+  for (const platform of allowedPlatforms) {
+    if (uri.includes(platform)) {
+      return true;
     }
-    return false;
-}
+  }
+  return false;
+};
 
-export { resolveIpfsUri, sanitizeUri, resolveCloudLinkUrl, getLivePlatformFromUri, resolveStreamingLink, isSubmitUriFromAllowedPlatforms, isLiveUriFromAllowedPlatforms };
+export {
+  resolveIpfsUri,
+  sanitizeUri,
+  resolveCloudLinkUrl,
+  getLivePlatformFromUri,
+  resolveStreamingLink,
+  isSubmitUriFromAllowedPlatforms,
+  isLiveUriFromAllowedPlatforms,
+};
